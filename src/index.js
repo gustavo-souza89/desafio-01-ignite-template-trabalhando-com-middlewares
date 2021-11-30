753f2b9a0d7e7f7fc19ebc547d1ad483de5e7961
Gustavo Souza <gustavo.souzagu@gmail.com>
2021-11-29 21:31:05 -0300

    Finalizando o Desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,87 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({
+      error: "User not found.",
+    });
+  }
+  request.user = user;
+
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
-}
+  const {user} = request
+
+  //const user = users.find(user => user.username === username)
+
+  if(!user.pro && user.todos.length >= 10)
+  {
+    return response.status(403).json({
+      error: "Todos limit reached. Please upgrade your plan!",
+    });
+  } 
+    return next();
+  }
+
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const{username} = request.headers;
+  const {id} = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "User not found.",});
+  }  
+
+  if(!validate(id)){
+    return response.status(400).json({
+      error: "ID is not a UUID",
+    });
+  }
+
+  const todoExists = user.todos.find(todo => todo.id === id);
+  if(!todoExists){
+    return response.status(404).json({
+      error: "Todo not found.",
+    });
+  }
+
+  request.todo = todoExists;
+  request.user = user;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+   const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: "ID is not a UUID",
+    });
+  }
+
+  const userExists = users.find((user) => user.id == id);
+  
+  if (!userExists) {
+    return response.status(404).json({
+      error: "User not found",
+    });   
+  }
+   
+  request.user = userExists;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
